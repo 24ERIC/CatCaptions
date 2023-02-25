89b5c7f3456937f7ed4ab486d159ef116a6a130f
Jason Co <jasonty33@gmail.com>
2023-02-25 10:02:02 -0500

    Update CustomInput.js

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 
-const CustomInput = ({}) => {
+const CustomInput = ({value, setValue, placeholder}) => {
     return (
         <View>
-
+            <TextInput
+                value={value}
+                onChangeText={setValue}
+                placeholder={placeholder}
+                style={styles.input}
+            >
+            </TextInput>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFFFFF',
+        width: '100%',
+        height: '7%',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginVertical: 5,
+        justifyContent: 'center'
+    },
+    input: {
+        fontSize: 24
+    },
+});
 
 export default CustomInput
