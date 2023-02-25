e088258e7f5345f953062e0cdd4654d0e763252c
bobbyta74 <bobbyta74@gmail.com>
2023-02-25 15:05:45 +0100

    Add CustomButton to TakePictureScreen (no function yet)

--- src/screens/TakePictureScreen/TakePictureScreen.js
+++ src/screens/TakePictureScreen/TakePictureScreen.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { Text, View, Image } from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import CustomButton from './src/components/CustomButton';
 
 const TakePictureScreen = () => {
+    const openCamera = () => {
+
+    }
 
     return (
         <View>
-            <Text>photo!</Text>
+            <CustomButton text="Choose from camera roll" onPress={() => {openCamera}}/>
         </View>
     );
 };
