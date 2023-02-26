f10e54913da338bb3a0b7eacaf0993616bf2df1c
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-02-25 20:50:18 -0500

    finish ShowPictureScreen.js with simple demo deisgn

--- src/screens/ShowPictureScreen/ShowPictureScreen.js
+++ src/screens/ShowPictureScreen/ShowPictureScreen.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
-import { Text, View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { Text, Avatar } from 'react-native-paper';
 
-const ShowPictureScreen = () => {
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
+
+export default function ShowPictureScreen() {
     return (
-        <View>
-            <Text>photo!</Text>
-        </View>
+      <View style={styles.container}>
+        <Avatar.Image source={require('./image.jpeg')} size={200} style={styles.image} />
+        <Text style={styles.text}>Me and my fave toy - BFFs 4 life!</Text>
+      </View>
     );
-};
-
-export default ShowPictureScreen
+  }
